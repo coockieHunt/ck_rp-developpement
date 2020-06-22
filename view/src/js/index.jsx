e3e73656f4c08aa893e4bfc26e70e08c5fe55fb8
Jonathan <jonathan.gleyze@gmail.com>
2020-06-22 08:32:21 +0200

    [update] repo structure

--- view/src/js/index.jsx
+++ view/src/js/index.jsx
@@ -1,9 +1,44 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+import '../css/index.css'
+
+class Incremennt extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = ( {curent_increment : this.props.start} )
+        this.timer = null
+    }
+
+    componentDidMount(){
+        this.timer =  window.setInterval(this.tick.bind(this), 1000)
+    }
+
+    componentwillUnmount(){
+        window.setTimeout(this.timer)
+    }
+
+    tick(){
+            this.setState(function (state, props){
+                var new_inc = this.state.curent_increment + this.props.step
+                if( new_inc > this.props.end ){ new_inc = this.props.end}
+                if( new_inc <= this.props.end ){
+                    return {curent_increment : new_inc}
+                }
+            })
+    }
+    
+    
+    render(){
+        return <span className="incr"> {this.state.curent_increment} </span>
+    }
+}
+
+
 
 function Home(){
     return <div>
         <p>hello world</p>
+        <Incremennt start={0} end={100} step={1}/>
     </div>
 }
 
