6bf73f3fb97bfd27168645495b73b773e881ef23
Jonathan <jonathan.gleyze@gmail.com>
2020-06-22 08:06:26 +0200

    first commit

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+function Home(){
+    return <div>
+        <p>hello</p>
+    </div>
+}
+
+ReactDOM.render(< Home />, document.querySelector('#app'))
